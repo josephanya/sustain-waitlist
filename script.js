14a256daa90a7bf92f8229cdfc0fbff06e4875e7
Joseph Anya <josephanya4real@gmail.com>
2021-02-06 22:18:27 +0100

    new commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 var formModal = document.getElementById("form-modal");
+var successModal = document.getElementById("success-modal");
 var openBtn = document.getElementById("open-form");
-
-// Get the <span> element that closes the modal
 var closeBtn= document.getElementsByClassName("close-modal")[0];
 
-// When the user clicks on the button, open the modal 
 openBtn.onclick = function () {
     formModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 closeBtn.onclick = function () {
     formModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == formModal) {
         formModal.style.display = "none";
     }
+}
+
+const submitData = () => {
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var email = document.getElementById('email').value;
+
+    var waitlistData = {
+        'firstName' : firstName,
+        'lastName' : lastName,
+        'email' : email
+    }    
 }
