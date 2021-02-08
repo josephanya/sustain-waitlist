e9d4483cefaac19322e35163f7f625387ff365a2
Joseph Anya <josephanya4real@gmail.com>
2021-02-08 05:14:00 +0100

    new commit

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-var formModal = document.getElementById("form-modal");
-var successModal = document.getElementById("success-modal");
-var openBtn = document.getElementById("open-form");
-var closeBtn= document.getElementsByClassName("close-modal")[0];
+const formModal = document.getElementById("form-modal");
+const successModal = document.getElementById("success-modal");
+const openBtn = document.getElementById("open-form");
+const closeBtn = document.getElementsByClassName("close-modal")[0];
+const joinBtn = document.getElementById("join-button");
 
-openBtn.onclick = function () {
-    formModal.style.display = "block";
-}
-
-closeBtn.onclick = function () {
-    formModal.style.display = "none";
-}
-
-window.onclick = function (event) {
+openBtn.addEventListener('click', () => formModal.style.display = "block");
+closeBtn.addEventListener('click', () => formModal.style.display = "none");
+window.addEventListener('click', (event) => {
     if (event.target == formModal) {
         formModal.style.display = "none";
     }
-}
+});
+
+joinBtn.addEventListener('click', submitData);
 
 const submitData = () => {
-    var firstName = document.getElementById('first-name').value;
-    var lastName = document.getElementById('last-name').value;
-    var email = document.getElementById('email').value;
-
-    var waitlistData = {
+    let firstName = document.getElementById('first-name').value;
+    let lastName = document.getElementById('last-name').value;
+    let email = document.getElementById('email').value;
+    let waitlistData = {
         'firstName' : firstName,
         'lastName' : lastName,
         'email' : email
-    }    
+    }
+    let token1 = atob("token1");
+    let token2 = atob("token2");
+    let token3 = atob("token3");
+    let urL = `https://hooks.slack.com/services/${token1}/${token2}/${token3}`
+    let payload = {"text": `${waitlistData.firstName} ${waitlistData.lastName} just joined the waitlist. Email: ${waitlistData.email}`}
+    let otherParam = {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    }
+
+    fetch(`https://cors-anywhere.herokuapp.com/${urL}`, otherParam)
+    .then(res => {
+        if(res.status == 200){
+
+        }else{
+
+        }
+        return res.json()
+    })
+    .catch(error =>{ 
+        console.log(error);
+    })
 }
